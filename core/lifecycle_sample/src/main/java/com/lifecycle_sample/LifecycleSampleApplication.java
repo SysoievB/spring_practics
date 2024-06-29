825a66dd3567d60deed1dc02b66ca8b14a824b61
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-06-29 07:22:29 +0200

    Added lifecycle annotation

---
+++ core/lifecycle_sample/src/main/java/com/lifecycle_sample/LifecycleSampleApplication.java
@@ -0,0 +1,13 @@
+package com.lifecycle_sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifecycleSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LifecycleSampleApplication.class, args);
+    }
+
+}
---
+++ core/lifecycle_sample/src/test/java/com/lifecycle_sample/LifecycleSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lifecycle_sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifecycleSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
