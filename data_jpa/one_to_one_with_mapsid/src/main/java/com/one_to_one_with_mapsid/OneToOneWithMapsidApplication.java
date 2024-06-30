d7fdec2e480d4e683af1168e662749032b5186f5
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-06-30 17:28:21 +0200

    Added OneToOne with MapsId annotation

---
+++ data_jpa/one_to_one_with_mapsid/src/main/java/com/one_to_one_with_mapsid/OneToOneWithMapsidApplication.java
@@ -0,0 +1,13 @@
+package com.one_to_one_with_mapsid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneToOneWithMapsidApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OneToOneWithMapsidApplication.class, args);
+    }
+
+}
---
+++ data_jpa/one_to_one_with_mapsid/src/test/java/com/one_to_one_with_mapsid/OneToOneWithMapsidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.one_to_one_with_mapsid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneToOneWithMapsidApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
