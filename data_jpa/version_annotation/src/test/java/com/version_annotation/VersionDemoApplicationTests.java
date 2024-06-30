aaf313ddc3aceb18991d801b586de088d5ee3bce
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-06-30 16:04:15 +0200

    Added OneToOne annotation

---
+++ data_jpa/version_annotation/src/test/java/com/version_annotation/VersionDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.version_annotation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VersionDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
