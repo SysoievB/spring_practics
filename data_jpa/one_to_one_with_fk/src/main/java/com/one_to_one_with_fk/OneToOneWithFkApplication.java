4f87f7dad94c73e1a8e4a3bdc820b935dfbee4c7
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-06-30 16:03:42 +0200

    Added OneToOne annotation

---
+++ data_jpa/one_to_one_with_fk/src/main/java/com/one_to_one_with_fk/OneToOneWithFkApplication.java
@@ -0,0 +1,13 @@
+package com.one_to_one_with_fk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneToOneWithFkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneToOneWithFkApplication.class, args);
+	}
+
+}
---
+++ data_jpa/one_to_one_with_fk/src/test/java/com/one_to_one_with_fk/OneToOneWithFkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.one_to_one_with_fk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneToOneWithFkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
