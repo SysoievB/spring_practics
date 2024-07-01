7e0318b56bd1e5cf7141dc4ac8aed14b2af12bb1
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-07-01 14:09:09 +0200

    Added OneToOne with JoinTable annotation

---
+++ data_jpa/one_to_one_with_jointable/src/test/java/com/one_to_one_with_jointable/OneToOneWithJointableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.one_to_one_with_jointable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneToOneWithJointableApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
