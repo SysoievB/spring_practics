502f9b7ca17ad4d8fe5e6959a7f6a9f3235b754c
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-07-01 06:03:49 +0200

    Added OneToOne with PrimaryKeyJoinColumn annotation

---
+++ data_jpa/one_to_one_primarykeyjoincolumn/src/main/java/com/one_to_one_primarykeyjoincolumn/OneToOnePrimarykeyjoincolumnApplication.java
@@ -0,0 +1,13 @@
+package com.one_to_one_primarykeyjoincolumn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneToOnePrimarykeyjoincolumnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneToOnePrimarykeyjoincolumnApplication.class, args);
+	}
+
+}
---
+++ data_jpa/one_to_one_primarykeyjoincolumn/src/test/java/com/one_to_one_primarykeyjoincolumn/OneToOnePrimarykeyjoincolumnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.one_to_one_primarykeyjoincolumn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneToOnePrimarykeyjoincolumnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
