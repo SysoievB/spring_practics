19bdf81c8909061952993406354cb16226461b5d
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-07-02 20:20:08 +0200

    Added orphan removal

---
+++ data_jpa/cascade_type_and_orhpan_removal/src/main/java/com/cascade_type_and_orhpan_removal/CascadeTypeAndOrhpanRemovalApplication.java
@@ -0,0 +1,13 @@
+package com.cascade_type_and_orhpan_removal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CascadeTypeAndOrhpanRemovalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CascadeTypeAndOrhpanRemovalApplication.class, args);
+    }
+
+}
---
+++ data_jpa/cascade_type_and_orhpan_removal/src/test/java/com/cascade_type_and_orhpan_removal/CascadeTypeAndOrhpanRemovalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cascade_type_and_orhpan_removal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CascadeTypeAndOrhpanRemovalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
