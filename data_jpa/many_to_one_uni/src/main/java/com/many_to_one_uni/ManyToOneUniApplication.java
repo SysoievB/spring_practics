449ec4fac35bafadf5671503310cc68aa4ce2828
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-07-02 19:30:16 +0200

    Added many to one unidirectional

---
+++ data_jpa/many_to_one_uni/src/main/java/com/many_to_one_uni/ManyToOneUniApplication.java
@@ -0,0 +1,13 @@
+package com.many_to_one_uni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManyToOneUniApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManyToOneUniApplication.class, args);
+    }
+
+}
---
+++ data_jpa/many_to_one_uni/src/test/java/com/many_to_one_uni/ManyToOneUniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.many_to_one_uni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManyToOneUniApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
