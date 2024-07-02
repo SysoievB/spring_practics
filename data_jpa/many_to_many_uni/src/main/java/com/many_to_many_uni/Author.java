1cc18758e1302d850c676212acd1ca1eda1d7b98
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-07-02 20:13:49 +0200

    Added many to many unidirectional

---
+++ data_jpa/many_to_many_uni/src/main/java/com/many_to_many_uni/Author.java
@@ -0,0 +1,22 @@
+package com.many_to_many_uni;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "author")
+@AllArgsConstructor
+@NoArgsConstructor
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+}
