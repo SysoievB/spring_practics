2f2483b24c1a47a370d574589a8e8cc324f20d21
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-07-03 17:06:39 +0200

    Added one to many bidirectional

---
+++ data_jpa/one_to_many_bi/src/main/java/com/one_to_many_bi/OneToManyBiApplication.java
@@ -0,0 +1,13 @@
+package com.one_to_many_bi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneToManyBiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OneToManyBiApplication.class, args);
+    }
+
+}
---
+++ data_jpa/one_to_many_bi/src/test/java/com/one_to_many_bi/OneToManyBiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.one_to_many_bi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneToManyBiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
