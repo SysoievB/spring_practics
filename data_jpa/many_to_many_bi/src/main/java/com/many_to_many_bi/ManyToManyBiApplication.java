1d5f07698cbea2db892198f52aeb0669d1507e24
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-07-03 17:21:35 +0200

    Added many to many bidirectional

---
+++ data_jpa/many_to_many_bi/src/main/java/com/many_to_many_bi/ManyToManyBiApplication.java
@@ -0,0 +1,13 @@
+package com.many_to_many_bi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManyToManyBiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManyToManyBiApplication.class, args);
+    }
+
+}
---
+++ data_jpa/many_to_many_bi/src/test/java/com/many_to_many_bi/ManyToManyBiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.many_to_many_bi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManyToManyBiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
