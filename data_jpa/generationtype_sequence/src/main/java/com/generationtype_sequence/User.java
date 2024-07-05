7765d0cfc9cc2e202298dd75fc210671cf3ad39c
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-07-05 07:55:30 +0200

    Added generetion type SEQUENCE

--- data_jpa/generationtype_sequence/src/main/java/com/generationtype_sequence/User.java
+++ data_jpa/generationtype_sequence/src/main/java/com/generationtype_sequence/User.java
@@ -2,25 +2,17 @@ package com.generationtype_sequence;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
-import org.hibernate.annotations.GenericGenerator;
-import org.hibernate.annotations.Parameter;
 
 @Entity
 @Table
 public class User {
     @Id
-    @GeneratedValue(generator = "sequence-generator")
-    @GenericGenerator(
-            name = "sequence-generator",
-            strategy = "org.hibernate.id.enhanced.SequenceStyleGenerator",
-            parameters = {
-                    @Parameter(name = "sequence_name", value = "user_sequence"),
-                    @Parameter(name = "initial_value", value = "4"),
-                    @Parameter(name = "increment_size", value = "1")
-            }
-    )
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_user")
+    @SequenceGenerator(name = "seq_user", allocationSize = 5)
     private Long id;
 
     private String name;
