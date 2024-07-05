efb13d3f41134bf567c748c72e7cad0cb9b771e3
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-07-05 07:47:34 +0200

    Added generetion type SEQUENCE

---
+++ data_jpa/generationtype_sequence/src/main/java/com/generationtype_sequence/GenerationtypeSequenceApplication.java
@@ -0,0 +1,13 @@
+package com.generationtype_sequence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenerationtypeSequenceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GenerationtypeSequenceApplication.class, args);
+    }
+
+}
