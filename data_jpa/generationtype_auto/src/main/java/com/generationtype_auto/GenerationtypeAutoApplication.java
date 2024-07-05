ca700f49366f85135858869eedd7ad01c8fa487e
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-07-05 05:55:28 +0200

    Added generation type AUTO

---
+++ data_jpa/generationtype_auto/src/main/java/com/generationtype_auto/GenerationtypeAutoApplication.java
@@ -0,0 +1,13 @@
+package com.generationtype_auto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenerationtypeAutoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenerationtypeAutoApplication.class, args);
+	}
+
+}
---
+++ data_jpa/generationtype_auto/src/test/java/com/generationtype_auto/GenerationtypeAutoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generationtype_auto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenerationtypeAutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
