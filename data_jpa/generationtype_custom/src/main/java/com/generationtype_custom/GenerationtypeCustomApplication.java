4b875d8f6cc971dbc0da288872a694c6811f3c32
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-07-06 06:46:01 +0200

    Added generationtype custom

---
+++ data_jpa/generationtype_custom/src/main/java/com/generationtype_custom/GenerationtypeCustomApplication.java
@@ -0,0 +1,13 @@
+package com.generationtype_custom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenerationtypeCustomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GenerationtypeCustomApplication.class, args);
+    }
+
+}
---
+++ data_jpa/generationtype_custom/src/test/java/com/generationtype_custom/GenerationtypeCustomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generationtype_custom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenerationtypeCustomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
