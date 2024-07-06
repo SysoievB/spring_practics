501e032c21ade8e89567ced2e5392e0c512a3afe
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-07-06 05:38:07 +0200

    Added generationtype table

---
+++ data_jpa/generationtype_table/src/main/java/com/generationtype_table/GenerationtypeTableApplication.java
@@ -0,0 +1,13 @@
+package com.generationtype_table;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenerationtypeTableApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GenerationtypeTableApplication.class, args);
+    }
+
+}
---
+++ data_jpa/generationtype_table/src/test/java/com/generationtype_table/GenerationtypeTableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generationtype_table;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenerationtypeTableApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
