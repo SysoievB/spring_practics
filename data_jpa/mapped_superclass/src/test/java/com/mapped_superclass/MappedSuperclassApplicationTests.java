c1052ebc2117f22781bd60321866c5754d23ce70
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-07-07 10:27:02 +0200

    Added MappedSuperclass annotation

---
+++ data_jpa/mapped_superclass/src/test/java/com/mapped_superclass/MappedSuperclassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mapped_superclass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MappedSuperclassApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
