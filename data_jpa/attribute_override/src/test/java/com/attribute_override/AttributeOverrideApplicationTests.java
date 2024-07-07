1197d614015e30a2736726340cd0b916a3191385
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-07-07 10:13:20 +0200

    Added AttributeOverrides annotation

---
+++ data_jpa/attribute_override/src/test/java/com/attribute_override/AttributeOverrideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.attribute_override;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttributeOverrideApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
