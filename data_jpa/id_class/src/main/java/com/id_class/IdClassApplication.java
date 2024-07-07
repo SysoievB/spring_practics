2b01ed3105d90c0d18fdd30f5f9674d37b6cc5da
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-07-07 08:57:43 +0200

    Added IdClass annotation

---
+++ data_jpa/id_class/src/main/java/com/id_class/IdClassApplication.java
@@ -0,0 +1,13 @@
+package com.id_class;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdClassApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdClassApplication.class, args);
+    }
+
+}
