2f55e1583c6c9ed38eda51b42fecdad5162dc53b
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-07-10 07:06:39 +0200

    Added Query annotation

---
+++ data_jpa/query_param_modifying/src/main/java/com/query_param_modifying/QueryParamModifyingApplication.java
@@ -0,0 +1,13 @@
+package com.query_param_modifying;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueryParamModifyingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QueryParamModifyingApplication.class, args);
+    }
+
+}
---
+++ data_jpa/query_param_modifying/src/test/java/com/query_param_modifying/QueryParamModifyingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.query_param_modifying;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueryParamModifyingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
