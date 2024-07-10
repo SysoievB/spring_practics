46d127015aaa3faf5e26ca6ef62eaea0aed74dc5
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-07-10 05:22:39 +0200

    Added NoRepositoryBean annotation

---
+++ data_jpa/no_repository_bean/src/main/java/com/no_repository_bean/NoRepositoryBeanApplication.java
@@ -0,0 +1,13 @@
+package com.no_repository_bean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoRepositoryBeanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoRepositoryBeanApplication.class, args);
+    }
+
+}
---
+++ data_jpa/no_repository_bean/src/test/java/com/no_repository_bean/NoRepositoryBeanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.no_repository_bean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoRepositoryBeanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
