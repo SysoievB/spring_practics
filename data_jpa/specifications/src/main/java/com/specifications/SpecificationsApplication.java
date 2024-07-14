00c81abcae78b8eab4e59ee1d1da4ef187d1a51a
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-07-14 11:36:17 +0200

    Added specifications

--- data_jpa/specifications/src/main/java/com/specifications/SpecificationsApplication.java
+++ data_jpa/specifications/src/main/java/com/specifications/SpecificationsApplication.java
@@ -1,13 +1,20 @@
 package com.specifications;
 
+import lombok.AllArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpecificationsApplication {
+@AllArgsConstructor
+public class SpecificationsApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(SpecificationsApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
 }
