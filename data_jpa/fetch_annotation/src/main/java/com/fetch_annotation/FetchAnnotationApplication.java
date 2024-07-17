7799f2da90c11305c5a65dcf3f42341046dad6c6
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-07-17 06:23:12 +0200

    Added fetch annotation

---
+++ data_jpa/fetch_annotation/src/main/java/com/fetch_annotation/FetchAnnotationApplication.java
@@ -0,0 +1,13 @@
+package com.fetch_annotation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FetchAnnotationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FetchAnnotationApplication.class, args);
+    }
+
+}
---
+++ data_jpa/fetch_annotation/src/test/java/com/fetch_annotation/FetchAnnotationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fetch_annotation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FetchAnnotationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
