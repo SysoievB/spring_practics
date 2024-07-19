52981ebcf873c49e23a139410b12a13dbfb00518
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-07-19 20:40:06 +0200

    Added @Formula

---
+++ data_jpa/formula_annotation/src/main/java/com/formula_annotation/FormulaAnnotationApplication.java
@@ -0,0 +1,13 @@
+package com.formula_annotation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormulaAnnotationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FormulaAnnotationApplication.class, args);
+    }
+
+}
---
+++ data_jpa/formula_annotation/src/test/java/com/formula_annotation/FormulaAnnotationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.formula_annotation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormulaAnnotationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
