82ac61021a5625b414fc6313da685ea854f15e40
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-07-19 07:10:43 +0200

    Added @Filter

---
+++ data_jpa/filter_annotation/src/main/java/com/filter_annotation/FilterAnnotationApplication.java
@@ -0,0 +1,13 @@
+package com.filter_annotation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilterAnnotationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FilterAnnotationApplication.class, args);
+    }
+
+}
---
+++ data_jpa/filter_annotation/src/test/java/com/filter_annotation/FilterAnnotationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filter_annotation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilterAnnotationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
