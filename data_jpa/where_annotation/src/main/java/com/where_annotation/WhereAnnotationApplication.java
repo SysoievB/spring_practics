1af5a78fddfcef5a07645378d96cdf7d697e6a40
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-07-20 10:18:54 +0200

    Added @Where

---
+++ data_jpa/where_annotation/src/main/java/com/where_annotation/WhereAnnotationApplication.java
@@ -0,0 +1,13 @@
+package com.where_annotation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhereAnnotationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhereAnnotationApplication.class, args);
+    }
+
+}
---
+++ data_jpa/where_annotation/src/test/java/com/where_annotation/WhereAnnotationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.where_annotation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhereAnnotationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
