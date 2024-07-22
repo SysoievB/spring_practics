4ab280267117c10e477301882ccd789623fbc1ef
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-07-22 15:14:45 +0200

    Added named entity graph

---
+++ data_jpa/named_entity_graph/src/main/java/com/named_entity_graph/NamedEntityGraphApplication.java
@@ -0,0 +1,13 @@
+package com.named_entity_graph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NamedEntityGraphApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NamedEntityGraphApplication.class, args);
+    }
+
+}
---
+++ data_jpa/named_entity_graph/src/test/java/com/named_entity_graph/NamedEntityGraphApplicationTests.java
@@ -0,0 +1,13 @@
+package com.named_entity_graph;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamedEntityGraphApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
