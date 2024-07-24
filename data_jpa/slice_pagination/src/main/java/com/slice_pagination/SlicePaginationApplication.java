1c4fce1477354885a28c52be6a40c11806a82111
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-07-24 10:49:22 +0200

    Added slice pagination

---
+++ data_jpa/slice_pagination/src/main/java/com/slice_pagination/SlicePaginationApplication.java
@@ -0,0 +1,13 @@
+package com.slice_pagination;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlicePaginationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlicePaginationApplication.class, args);
+    }
+
+}
---
+++ data_jpa/slice_pagination/src/test/java/com/slice_pagination/SlicePaginationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slice_pagination;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlicePaginationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
