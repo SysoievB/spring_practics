c50d4c409f200706765036c71bab2d263e4856b4
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-07-24 05:28:23 +0200

    Added keyset pagination

---
+++ data_jpa/keyset_pagination/src/test/java/com/keyset_pagination/KeysetPaginationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keyset_pagination;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeysetPaginationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
