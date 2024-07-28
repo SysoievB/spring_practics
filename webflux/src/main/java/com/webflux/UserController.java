d9a7e100dbadf0ff9c26d4b1128591fe7cf84528
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-07-28 13:02:09 +0200

    Added webflux

--- webflux/src/main/java/com/webflux/UserController.java
+++ webflux/src/main/java/com/webflux/UserController.java
@@ -1,9 +1,19 @@
 package com.webflux;
 
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/user")
@@ -11,4 +21,31 @@ import org.springframework.web.bind.annotation.RestController;
 @AllArgsConstructor
 public class UserController {
     private final UserService service;
+
+    @GetMapping
+    public Mono<List<User>> getAll() {
+        return service.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public Mono<ResponseEntity<User>> getProductById(@PathVariable Long id) {
+        return service.findUserById(id)
+                .map(ResponseEntity::ok)
+                .defaultIfEmpty(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public Mono<User> createProduct(@RequestBody UserDto dto) {
+        return service.createUser(dto);
+    }
+
+    @PutMapping("/{id}")
+    public Mono<User> updateProduct(@PathVariable Long id, @RequestBody UserDto dto) {
+        return service.updateUser(dto, id);
+    }
+
+    @DeleteMapping("{id}")
+    public Mono<Void> deleteProduct(@PathVariable Long id) {
+        return service.deleteUser(id);
+    }
 }
--- webflux/src/main/java/com/webflux/UserDto.java
+++ webflux/src/main/java/com/webflux/UserDto.java
@@ -5,7 +5,7 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
-record UserDto(
+public record UserDto(
         @NotBlank String name,
         @Min(value = 18) @Max(value = 80) @NotNull Integer age
 ) {
