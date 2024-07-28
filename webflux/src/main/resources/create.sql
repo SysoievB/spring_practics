6f13f5d8ef9ded4c2508c89bc641fce631912c55
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-07-28 13:11:47 +0200

    Added webflux

---
+++ webflux/src/main/resources/create.sql
@@ -0,0 +1,18 @@
+create table user
+(
+    id   bigint primary key auto_increment,
+    name varchar(255),
+    age  int
+);
+
+INSERT INTO user (name, age)
+VALUES ('John Doe', 25),
+       ('Jane Smith', 30),
+       ('Alice Johnson', 22),
+       ('Bob Brown', 28),
+       ('Charlie Davis', 35),
+       ('Diana Evans', 27),
+       ('Edward Frank', 31),
+       ('Fiona Green', 24),
+       ('George Harris', 29),
+       ('Hannah Ivy', 26);
