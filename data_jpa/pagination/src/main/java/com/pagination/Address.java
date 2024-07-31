0d4b886e1b222b77bb3577fafd1adb16427f20f5
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-07-31 07:48:41 +0200

    Added pagination

--- data_jpa/pagination/src/main/java/com/pagination/Address.java
+++ data_jpa/pagination/src/main/java/com/pagination/Address.java
@@ -1,22 +1,30 @@
 package com.pagination;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.ToString;
+import lombok.experimental.FieldDefaults;
 
 @Entity
 @Table(name = "address")
 @Getter
+@Setter
 @ToString
 @AllArgsConstructor
 @NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class Address {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
 
     String street;
