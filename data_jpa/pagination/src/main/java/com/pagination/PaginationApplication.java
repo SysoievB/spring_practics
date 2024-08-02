4b04b1a62c80485f7533fdac8a949071db2a9835
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-08-02 09:28:18 +0200

    Added pagination

--- data_jpa/pagination/src/main/java/com/pagination/PaginationApplication.java
+++ data_jpa/pagination/src/main/java/com/pagination/PaginationApplication.java
@@ -1,7 +1,12 @@
 package com.pagination;
 
+import lombok.val;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 
 @SpringBootApplication
 public class PaginationApplication {
@@ -10,4 +15,34 @@ public class PaginationApplication {
         SpringApplication.run(PaginationApplication.class, args);
     }
 
+    @Bean
+    CommandLineRunner startUp(AddressService service) {
+        return args -> {//todo check if pageable.unpaged()
+            /*Pageable sortedByName =
+  PageRequest.of(0, 3, Sort.by("name"));
+
+Pageable sortedByPriceDesc =
+  PageRequest.of(0, 3, Sort.by("price").descending());
+
+Pageable sortedByPriceDescNameAsc =
+  PageRequest.of(0, 5, Sort.by("price").descending().and(Sort.by("name")));*/
+            System.out.println("---getPageAll---");
+            val pageable = PageRequest.of(0, 3, Sort.by("id").ascending());
+            service.getPageAll(pageable)
+                    .forEach(address -> System.out.println(address.toString()));
+
+            System.out.println("---searchAddressesByCity---");
+            val cityPageable = PageRequest.of(0, 2, Sort.by("id").descending());
+            service.searchAddressesByCity("London", cityPageable)
+                    .forEach(address -> System.out.println(address.toString()));
+
+            System.out.println("---findAddressByCityWithPagination---");
+            service.findAddressByCityWithPagination(10, 0)
+                    .forEach(address -> System.out.println(address.toString()));
+
+            System.out.println("---findByCityWithPagination---");
+            service.findByCityWithPagination("London", cityPageable)
+                    .forEach(address -> System.out.println(address.toString()));
+        };
+    }
 }
