d6846fee1e10c4fb1b7b6177c57695648f34fb62
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-08-02 06:19:48 +0200

    Added pagination

--- data_jpa/pagination/src/main/java/com/pagination/AddressRepo.java
+++ data_jpa/pagination/src/main/java/com/pagination/AddressRepo.java
@@ -1,6 +1,24 @@
 package com.pagination;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 
-public interface AddressRepo extends JpaRepository<Address, Long> {
+import java.util.List;
+
+public interface AddressRepo extends PagingAndSortingRepository<Address, Long> {
+
+    Page<Address> findAll(Pageable pageable);
+
+    Page<Address> findByCityContaining(String city, Pageable pageable);
+
+    @Query(value = "SELECT * FROM pagination.address WHERE city = 'Los Angeles' LIMIT :limit OFFSET :offset", nativeQuery = true)
+    List<Address> findAddressByCityWithPagination(@Param("limit") int limit, @Param("offset") int offset);
+
+    @Query(value = "SELECT * FROM pagination.address WHERE city = :city",
+            countQuery = "SELECT count(*) FROM pagination.address WHERE city = :city",
+            nativeQuery = true)
+    Page<Address> findByCityWithPagination(@Param("city") String city, Pageable pageable);
 }
