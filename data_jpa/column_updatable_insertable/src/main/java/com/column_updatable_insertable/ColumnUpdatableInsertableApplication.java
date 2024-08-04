a1e3593922337fba149aa90e74d711edb265ebbf
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-08-04 09:15:06 +0200

    Added @Column insertable updatable

---
+++ data_jpa/column_updatable_insertable/src/main/java/com/column_updatable_insertable/ColumnUpdatableInsertableApplication.java
@@ -0,0 +1,13 @@
+package com.column_updatable_insertable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColumnUpdatableInsertableApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ColumnUpdatableInsertableApplication.class, args);
+    }
+
+}
