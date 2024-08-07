c74e150c507cad0b4e64cb67f854651c7c43556b
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-08-07 18:08:26 +0200

    Added @Immutable

---
+++ data_jpa/immutable_annotation/src/main/java/com/immutable_annotation/ImmutableAnnotationApplication.java
@@ -0,0 +1,13 @@
+package com.immutable_annotation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImmutableAnnotationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImmutableAnnotationApplication.class, args);
+    }
+
+}
---
+++ data_jpa/immutable_annotation/src/test/java/com/immutable_annotation/ImmutableAnnotationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.immutable_annotation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImmutableAnnotationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
