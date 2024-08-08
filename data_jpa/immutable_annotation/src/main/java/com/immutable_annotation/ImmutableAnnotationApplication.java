a84e33d49334429e122139bae546bad80a8557ef
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-08-08 06:41:40 +0200

    Added @Immutable

--- data_jpa/immutable_annotation/src/main/java/com/immutable_annotation/ImmutableAnnotationApplication.java
+++ data_jpa/immutable_annotation/src/main/java/com/immutable_annotation/ImmutableAnnotationApplication.java
@@ -1,7 +1,10 @@
 package com.immutable_annotation;
 
+import lombok.val;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ImmutableAnnotationApplication {
@@ -10,4 +13,23 @@ public class ImmutableAnnotationApplication {
         SpringApplication.run(ImmutableAnnotationApplication.class, args);
     }
 
+    @Bean
+    CommandLineRunner startUp(AddressRepo repo) {
+        return args -> {
+            val address = repo.findById(1L);
+            address.ifPresentOrElse(
+                    System.out::println,
+                    () -> System.out.println("Address with id = 1L not present")
+            );
+            //Address(id=1, street=Main Street, city=New York, flatNumber=1)
+
+            address.map(a -> {
+                        a.setCity("New City");
+                        return a;
+                    })
+                    .map(repo::save)
+                    .ifPresent(System.out::println);
+            //Address(id=1, street=Main Street, city=New York, flatNumber=1)
+        };
+    }
 }
