a983180274a905f03a3d1a6b100be07bc6bae8a0
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-08-10 06:44:39 +0200

    Added @NotNull vs @Column(nullable = false)

---
+++ data_jpa/notnull_annotation_vs_column_nullable_false/src/main/java/com/notnull_annotation_vs_column_nullable_false/NotnullAnnotationVsColumnNullableFalseApplication.java
@@ -0,0 +1,13 @@
+package com.notnull_annotation_vs_column_nullable_false;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotnullAnnotationVsColumnNullableFalseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotnullAnnotationVsColumnNullableFalseApplication.class, args);
+	}
+
+}
---
+++ data_jpa/notnull_annotation_vs_column_nullable_false/src/test/java/com/notnull_annotation_vs_column_nullable_false/NotnullAnnotationVsColumnNullableFalseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notnull_annotation_vs_column_nullable_false;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotnullAnnotationVsColumnNullableFalseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
