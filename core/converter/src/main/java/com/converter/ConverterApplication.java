87dffd4d50f327afc3f6a3cb727f4283b1b9e2ab
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-08-11 12:48:39 +0200

    Added converter

--- core/converter/src/main/java/com/converter/ConverterApplication.java
+++ core/converter/src/main/java/com/converter/ConverterApplication.java
@@ -1,13 +1,35 @@
 package com.converter;
 
+import lombok.val;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDate;
 
 @SpringBootApplication
 public class ConverterApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ConverterApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ConverterApplication.class, args);
+    }
 
+    @Bean
+    CommandLineRunner startUp(UserConverter converter) {
+        return args -> {
+            val user = new User(
+                    1L,
+                    "user123",
+                    "verySecurePassword",
+                    "Vasia Pupkin",
+                    LocalDate.now()
+            );
+			System.out.println(user);
+
+			System.out.println(converter.convert(user));
+            //User(id=1, username=user123, password=verySecurePassword, fullName=Vasia Pupkin, lastLogin=2024-08-11)
+            //UserDTO[username=user123, password=dmVyeVNlY3VyZVBhc3N3b3Jk]
+		};
+    }
 }
