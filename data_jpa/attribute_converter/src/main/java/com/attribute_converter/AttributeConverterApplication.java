f3a0f323bde582834a86528632ae357be9baefd9
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-08-12 08:48:58 +0200

    Added attribute converter

--- data_jpa/attribute_converter/src/main/java/com/attribute_converter/AttributeConverterApplication.java
+++ data_jpa/attribute_converter/src/main/java/com/attribute_converter/AttributeConverterApplication.java
@@ -1,13 +1,25 @@
 package com.attribute_converter;
 
+import lombok.val;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AttributeConverterApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AttributeConverterApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(EmailRepo repo) {
+        return args -> {
+            val email = new Email("Hello, how are you?", "Vasia Pupkin");
+            System.out.println(email);
+            System.out.println(repo.save(email));
+        };
     }
 
 }
