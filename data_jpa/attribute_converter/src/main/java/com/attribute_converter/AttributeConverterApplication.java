38e1d556b6b4297bd73ca1f2c4cf3692f6d776ca
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-08-12 06:10:36 +0200

    Added attribute converter

---
+++ data_jpa/attribute_converter/src/main/java/com/attribute_converter/AttributeConverterApplication.java
@@ -0,0 +1,13 @@
+package com.attribute_converter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttributeConverterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AttributeConverterApplication.class, args);
+    }
+
+}
