a22fadd80e2ffd6576fd69543735e27bf29f75d6
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-08-14 07:19:23 +0200

    Added optimistic locking

---
+++ data_jpa/optimistic_lock_exception/src/main/java/com/optimistic_lock_exception/OptimisticLockExceptionApplication.java
@@ -0,0 +1,13 @@
+package com.optimistic_lock_exception;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptimisticLockExceptionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OptimisticLockExceptionApplication.class, args);
+    }
+
+}
