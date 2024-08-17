4f0faff27c729c8e929794811f116b6fd56d465b
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-08-17 07:17:54 +0200

    Added pessimistic locking

---
+++ data_jpa/pessimistic_locking/src/main/java/com/pessimistic_locking/Product.java
@@ -0,0 +1,40 @@
+package com.pessimistic_locking;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Version;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "products")
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private Double price;
+
+    @Version
+    private Integer version;
+
+    public Product(String name, Double price) {
+        this.name = name;
+        this.price = price;
+    }
+}
+
