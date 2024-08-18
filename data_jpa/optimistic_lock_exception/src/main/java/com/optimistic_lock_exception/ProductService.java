b33d8ec80be2db107becea7d6676d3b191373457
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-08-18 12:05:06 +0200

    Added optimistic locking

--- data_jpa/optimistic_lock_exception/src/main/java/com/optimistic_lock_exception/ProductService.java
+++ data_jpa/optimistic_lock_exception/src/main/java/com/optimistic_lock_exception/ProductService.java
@@ -1,6 +1,7 @@
 package com.optimistic_lock_exception;
 
 import lombok.AllArgsConstructor;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
