b23b5b9277fa2ff0d74dfde1700510313ee38679
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-08-18 12:04:53 +0200

    Added optimistic locking

--- data_jpa/optimistic_lock_exception/src/main/java/com/optimistic_lock_exception/ProductRepository.java
+++ data_jpa/optimistic_lock_exception/src/main/java/com/optimistic_lock_exception/ProductRepository.java
@@ -1,7 +1,14 @@
 package com.optimistic_lock_exception;
 
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+
+import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    @Lock(LockModeType.OPTIMISTIC_FORCE_INCREMENT)
+    Optional<Product> findById(Long id);
 }
 
