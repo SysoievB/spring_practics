c4e90232a9ea1d6b973acf9dbfafd700de4dc08c
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-08-22 05:52:59 +0200

    Added headers

---
+++ web_headers/src/main/java/com/web_headers/WebHeadersApplication.java
@@ -0,0 +1,13 @@
+package com.web_headers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebHeadersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebHeadersApplication.class, args);
+    }
+
+}
---
+++ web_headers/src/test/java/com/web_headers/WebHeadersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web_headers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebHeadersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
