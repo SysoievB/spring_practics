56d8c3339d1f639fb84a37bfb1e02e832d120696
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-08-28 05:36:40 +0200

    Added profiles

---
+++ spring_profiles/src/main/java/com/spring_profiles/SpringProfilesApplication.java
@@ -0,0 +1,13 @@
+package com.spring_profiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProfilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProfilesApplication.class, args);
+	}
+
+}
