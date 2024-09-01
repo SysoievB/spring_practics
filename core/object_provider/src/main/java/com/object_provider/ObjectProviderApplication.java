3b7bedd949d74f8fa55c243afd02b191b1e0465a
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-09-01 12:18:41 +0200

    Added object provider

---
+++ core/object_provider/src/main/java/com/object_provider/ObjectProviderApplication.java
@@ -0,0 +1,13 @@
+package com.object_provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObjectProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ObjectProviderApplication.class, args);
+    }
+
+}
---
+++ core/object_provider/src/test/java/com/object_provider/ObjectProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.object_provider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObjectProviderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
