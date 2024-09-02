d42b5413f3843c1f27a92aa3dc5bc7db6b5c8ce7
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-09-02 06:29:52 +0200

    Added object provider

---
+++ core/object_provider/src/main/java/com/object_provider/EmployeeController.java
@@ -0,0 +1,36 @@
+package com.object_provider;
+
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+@AllArgsConstructor
+public class EmployeeController {
+    private final ObjectProvider<EmployeeService> employeeServices;
+
+    @GetMapping
+    public String getEmployee(@RequestParam WorkType workType) {
+        EmployeeService service = employeeServices.orderedStream()
+                .filter(s -> s.getWorkType().equals(workType))
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("No service found for the given work type"));
+
+        Employee employee = service.getFirstEmployeeByWorkType();
+        return service.getEmployeeName(employee);
+    }
+
+    @GetMapping("/available")
+    public String getIfAvailable() {
+        return employeeServices.getIfAvailable(RemoteEmployeeService::new).toString();
+    }//NoUniqueBeanDefinitionException
+
+    @GetMapping("/unique")
+    public String getIfUnique() {
+        return employeeServices.getIfUnique(OnsiteEmployeeService::new).toString();
+    }
+}
