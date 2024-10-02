d4ee87dfa18ad9bb0260aea3582f74f6fcb9e3a7
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-10-02 17:52:47 +0200

    Revert "Cleaned project"
    This reverts commit c932cbbf8cbef2847d31e6ea16bc16a8b7ec9f14.

---
+++ src/main/java/com/spring_practics/SpringPracticsApplication.java
@@ -0,0 +1,13 @@
+package com.spring_practics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPracticsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPracticsApplication.class, args);
+    }
+
+}
