2711867c2e1c496d1aef81d344eeafdc1782f6d8
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-10-25 08:34:02 +0200

    Added spring boot REST CRUD with virtual threads

---
+++ spring_boot_rest_crud_with_virtual_threads/src/main/java/com/spring_boot_rest_crud_with_virtual_threads/SpringBootRestCrudWithVirtualThreadsApplication.java
@@ -0,0 +1,13 @@
+package com.spring_boot_rest_crud_with_virtual_threads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestCrudWithVirtualThreadsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRestCrudWithVirtualThreadsApplication.class, args);
+    }
+
+}
---
+++ spring_boot_rest_crud_with_virtual_threads/src/test/java/com/spring_boot_rest_crud_with_virtual_threads/SpringBootRestCrudWithVirtualThreadsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_boot_rest_crud_with_virtual_threads;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestCrudWithVirtualThreadsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
