1c9bbb98dc2d722e612953d0cddba1e3735235fd
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-10-25 14:42:07 +0200

    Added spring boot REST CRUD with virtual threads

--- spring_boot_rest_crud_with_virtual_threads/src/main/java/com/spring_boot_rest_crud_with_virtual_threads/CarController.java
+++ spring_boot_rest_crud_with_virtual_threads/src/main/java/com/spring_boot_rest_crud_with_virtual_threads/CarController.java
@@ -1,6 +1,7 @@
 package com.spring_boot_rest_crud_with_virtual_threads;
 
 import lombok.RequiredArgsConstructor;
+import lombok.val;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -29,5 +30,13 @@ public class CarController {
     @DeleteMapping
     public void deleteCarByModel(@RequestParam String model) {
         carService.deleteCarByModel(model);
+    }
+
+    @GetMapping("/thread-type")
+    public String getThreadType() {
+        val threadName = Thread.currentThread().getName();
+        return Thread.currentThread().isVirtual()
+                ? threadName + " is virtual"
+                : threadName + " is regular";
     }
 }
