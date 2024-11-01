4c7c03f87fb38cb74cfaec13b30516c400d873d8
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-11-01 09:18:09 +0100

    Added jackson JsonIgnoreProperties

---
+++ jackson_annotations/src/main/java/com/jackson/JsonIgnorePropertiesClass.java
@@ -0,0 +1,30 @@
+package com.jackson;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.*;
+
+public class JsonIgnorePropertiesClass {
+    public static void main(String[] args) throws JsonProcessingException {
+        val mapper = new ObjectMapper();
+        val car = new Car("yellow", "renault", "ignored");
+        val json = mapper.writeValueAsString(car);//{"color":"yellow","type":"renault"}
+        System.out.println(json);
+
+        Car car1 = mapper.readValue(json, Car.class);
+        System.out.println(car1);
+        //JsonIgnorePropertiesClass.Car(color=yellow, type=renault, ignoredField=null)
+    }
+
+    @ToString
+    @Getter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @JsonIgnoreProperties({"ignoredField"})
+    private static class Car {
+        private String color;
+        private String type;
+        private String ignoredField;
+    }
+}
