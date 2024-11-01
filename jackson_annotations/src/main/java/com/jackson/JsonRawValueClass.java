372d2c8269c6cece3a4908284cc5850e468f2998
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-11-01 07:54:54 +0100

    Added jackson JsonRawValue

---
+++ jackson_annotations/src/main/java/com/jackson/JsonRawValueClass.java
@@ -0,0 +1,36 @@
+package com.jackson;
+
+import com.fasterxml.jackson.annotation.JsonRawValue;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+public class JsonRawValueClass {
+    public static void main(String[] args) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        Car car = new Car("yellow", "{\"brand\": \"renault\", \"model\": \"clio\"}");
+
+        String json = mapper.writeValueAsString(car);
+        System.out.println(json);
+/*
+* {
+    "color": "yellow",
+    "type": {
+        "brand": "renault",
+        "model": "clio"
+    }
+}*/
+    }
+
+    @ToString
+    @AllArgsConstructor
+    @NoArgsConstructor
+    private static class Car {
+        public String color;
+
+        @JsonRawValue
+        public String type;
+    }
+}
