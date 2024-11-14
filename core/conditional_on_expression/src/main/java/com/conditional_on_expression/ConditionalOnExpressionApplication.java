698eb3057d19cd728e5b687c4710a91b2fdc9a16
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-11-14 05:03:37 +0100

    Added conditionalOnExpression

---
+++ core/conditional_on_expression/src/main/java/com/conditional_on_expression/ConditionalOnExpressionApplication.java
@@ -0,0 +1,13 @@
+package com.conditional_on_expression;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConditionalOnExpressionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConditionalOnExpressionApplication.class, args);
+    }
+
+}
---
+++ core/conditional_on_expression/src/test/java/com/conditional_on_expression/ConditionalOnExpressionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.conditional_on_expression;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConditionalOnExpressionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
