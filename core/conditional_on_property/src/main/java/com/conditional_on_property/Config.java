19841b5ce2701979172e1b90305186f4d626ed96
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-11-15 08:06:45 +0100

    Added matchIfMissing = true explanation

--- core/conditional_on_property/src/main/java/com/conditional_on_property/Config.java
+++ core/conditional_on_property/src/main/java/com/conditional_on_property/Config.java
@@ -6,7 +6,12 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class Config {
-
+/**
+ *   <h3>matchIfMissing = true</h3>
+ *   If we commented my.service.enabled condition will match anyway.
+ *   If we will remove value from properties -> my.service.enabled=
+ *   -> application won`t even start.
+ *   */
     @Bean
     @ConditionalOnProperty(name = "my.service.enabled", havingValue = "true", matchIfMissing = true)
     MyService myService() {
