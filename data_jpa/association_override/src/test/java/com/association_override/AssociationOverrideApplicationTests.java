c4e9de80d7b38c865d96f2dabcd718a40f9bc02f
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-11-19 08:29:57 +0100

    Added @AssociationOverride

---
+++ data_jpa/association_override/src/test/java/com/association_override/AssociationOverrideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.association_override;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssociationOverrideApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
