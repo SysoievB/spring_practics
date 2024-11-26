f83a1c0728ff4973596a7792e5fe55192c342b1e
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-11-26 19:31:29 +0100

    Added @ContextConfiguration

---
+++ spring_tests/context_configuration/src/main/java/com/context_configuration/ContextConfigurationApplication.java
@@ -0,0 +1,13 @@
+package com.context_configuration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContextConfigurationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContextConfigurationApplication.class, args);
+    }
+
+}
---
+++ spring_tests/context_configuration/src/test/java/com/context_configuration/ContextConfigurationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.context_configuration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContextConfigurationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
