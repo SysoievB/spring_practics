e9b8c7be6a860edce7867a3f3082b4bcc0ed8367
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-11-26 05:30:25 +0100

    Added dirty context tests

---
+++ spring_tests/dirty_context/src/main/java/com/dirty_context/DirtyContextApplication.java
@@ -0,0 +1,13 @@
+package com.dirty_context;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DirtyContextApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DirtyContextApplication.class, args);
+    }
+
+}
