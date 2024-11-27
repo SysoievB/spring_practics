30a2ec1ad6340281841aebf1aa44fa43e47f37e3
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-11-27 04:59:37 +0100

    Added @TestConfiguration

---
+++ spring_tests/test_configuration/src/main/java/com/test_configuration/TestConfigurationApplication.java
@@ -0,0 +1,13 @@
+package com.test_configuration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestConfigurationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestConfigurationApplication.class, args);
+	}
+
+}
