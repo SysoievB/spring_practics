1c2189cc481446cf062a23e039559d18c1b2054d
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-11-28 05:41:08 +0100

    Added @DynamicPropertySource

---
+++ spring_tests/dynamic_property_source/src/main/java/com/dynamic_property_source/DynamicPropertySourceApplication.java
@@ -0,0 +1,13 @@
+package com.dynamic_property_source;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicPropertySourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DynamicPropertySourceApplication.class, args);
+    }
+
+}
---
+++ spring_tests/dynamic_property_source/src/test/java/com/dynamic_property_source/DynamicPropertySourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dynamic_property_source;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamicPropertySourceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
