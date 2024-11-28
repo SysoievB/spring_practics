aeab1e72c0ab5645781ae19bd68ce6e087bb9768
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-11-28 05:31:30 +0100

    Added @TestPropertySource

---
+++ spring_tests/test_property_source/src/main/java/com/test_property_source/TestPropertySourceApplication.java
@@ -0,0 +1,13 @@
+package com.test_property_source;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestPropertySourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestPropertySourceApplication.class, args);
+    }
+
+}
---
+++ spring_tests/test_property_source/src/test/java/com/test_property_source/TestPropertySourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test_property_source;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestPropertySourceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
