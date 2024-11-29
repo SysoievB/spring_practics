525578da6dab60c593c1d612a0268e17a6509ec8
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-11-29 06:18:44 +0100

    Added @TestBean

---
+++ spring_tests/test_bean/src/test/java/com/test_bean/TestBeanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test_bean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestBeanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
