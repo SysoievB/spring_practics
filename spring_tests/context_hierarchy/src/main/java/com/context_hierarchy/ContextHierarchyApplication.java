60178671d7d88f7bcce2b578b96a0f9c6857dd37
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-12-08 13:10:07 +0100

    Added @ContextHierarchy

---
+++ spring_tests/context_hierarchy/src/main/java/com/context_hierarchy/ContextHierarchyApplication.java
@@ -0,0 +1,13 @@
+package com.context_hierarchy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContextHierarchyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContextHierarchyApplication.class, args);
+    }
+
+}
---
+++ spring_tests/context_hierarchy/src/test/java/com/context_hierarchy/ContextHierarchyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.context_hierarchy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContextHierarchyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
