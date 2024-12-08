2339e581b6170c48afd9ab1cd17757b2196eab64
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-12-08 13:45:48 +0100

    Added @WebAppConfiguration

---
+++ spring_tests/web_app_configuration/src/main/java/com/web_app_configuration/WebAppConfigurationApplication.java
@@ -0,0 +1,13 @@
+package com.web_app_configuration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAppConfigurationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebAppConfigurationApplication.class, args);
+    }
+
+}
---
+++ spring_tests/web_app_configuration/src/test/java/com/web_app_configuration/WebAppConfigurationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web_app_configuration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebAppConfigurationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
