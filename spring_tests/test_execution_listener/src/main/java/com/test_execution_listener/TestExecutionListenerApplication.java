c040067155510697b5236da3f68a2e27c4d3d2d7
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-12-09 07:51:36 +0100

    Added @TestExecutionListeners

---
+++ spring_tests/test_execution_listener/src/main/java/com/test_execution_listener/TestExecutionListenerApplication.java
@@ -0,0 +1,13 @@
+package com.test_execution_listener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestExecutionListenerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestExecutionListenerApplication.class, args);
+    }
+
+}
---
+++ spring_tests/test_execution_listener/src/test/java/com/test_execution_listener/TestExecutionListenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test_execution_listener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestExecutionListenerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
