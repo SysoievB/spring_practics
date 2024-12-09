32385dc4744d34d489a188253e73fc3af4ddc6de
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-12-09 07:47:03 +0100

    Added @DynamicPropertySource comment

--- spring_tests/test_property_source/src/test/java/com/test_property_source/MyDynamicServiceTest.java
+++ spring_tests/test_property_source/src/test/java/com/test_property_source/MyDynamicServiceTest.java
@@ -16,6 +16,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 })
 public class MyDynamicServiceTest {
 
+    /**
+     * <h3>@DynamicPropertySource</h3> is an annotation that can be applied to methods in integration
+     * test classes that need to register dynamic properties to be added to the set of
+     * PropertySources in the Environment for an ApplicationContext loaded for an integration
+     * test. Dynamic properties are useful when you do not know the value of the properties
+     * upfront – for example, if the properties are managed by an external resource such as
+     * for a container managed by the Testcontainers project.
+     * */
     @DynamicPropertySource
     static void dynamicProperties(DynamicPropertyRegistry registry) {
         registry.add("dynamic.property", () -> "dynamicValue");
