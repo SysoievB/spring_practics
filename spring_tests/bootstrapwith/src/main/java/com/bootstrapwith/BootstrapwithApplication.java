e0e1d06cd2e4024f8e1e0f63233cc09e498d9579
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-12-09 05:51:36 +0100

    Added @BootstrapWith

---
+++ spring_tests/bootstrapwith/src/main/java/com/bootstrapwith/BootstrapwithApplication.java
@@ -0,0 +1,13 @@
+package com.bootstrapwith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootstrapwithApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootstrapwithApplication.class, args);
+    }
+
+}
---
+++ spring_tests/bootstrapwith/src/test/java/com/bootstrapwith/BootstrapwithApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootstrapwith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootstrapwithApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
