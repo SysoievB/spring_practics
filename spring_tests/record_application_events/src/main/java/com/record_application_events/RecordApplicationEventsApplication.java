52d1b905cead774b6fe67f1bea20837bdc70cd18
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-12-13 06:37:05 +0100

    Added application events tests

---
+++ spring_tests/record_application_events/src/main/java/com/record_application_events/RecordApplicationEventsApplication.java
@@ -0,0 +1,13 @@
+package com.record_application_events;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecordApplicationEventsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecordApplicationEventsApplication.class, args);
+	}
+
+}
---
+++ spring_tests/record_application_events/src/test/java/com/record_application_events/RecordApplicationEventsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.record_application_events;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecordApplicationEventsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
