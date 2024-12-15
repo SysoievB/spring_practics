e3186e4810425c6e7320b436c478dc975efd2e05
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-12-15 10:14:33 +0100

    Added sql annotations tests

---
+++ spring_tests/sql_annotations/src/main/java/com/sql_annotations/SqlAnnotationsApplication.java
@@ -0,0 +1,13 @@
+package com.sql_annotations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlAnnotationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqlAnnotationsApplication.class, args);
+	}
+
+}
---
+++ spring_tests/sql_annotations/src/test/java/com/sql_annotations/SqlAnnotationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sql_annotations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqlAnnotationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
