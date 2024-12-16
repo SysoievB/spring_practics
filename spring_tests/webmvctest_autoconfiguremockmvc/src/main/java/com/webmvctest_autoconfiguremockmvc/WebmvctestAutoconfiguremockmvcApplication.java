c071f84ebd47b9c2641d93e1a349200bfe449377
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-12-16 20:50:59 +0100

    Added controller & integration tests

---
+++ spring_tests/webmvctest_autoconfiguremockmvc/src/main/java/com/webmvctest_autoconfiguremockmvc/WebmvctestAutoconfiguremockmvcApplication.java
@@ -0,0 +1,13 @@
+package com.webmvctest_autoconfiguremockmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebmvctestAutoconfiguremockmvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebmvctestAutoconfiguremockmvcApplication.class, args);
+	}
+
+}
---
+++ spring_tests/webmvctest_autoconfiguremockmvc/src/test/java/com/webmvctest_autoconfiguremockmvc/WebmvctestAutoconfiguremockmvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webmvctest_autoconfiguremockmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebmvctestAutoconfiguremockmvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
