5da0c74c011a521d6b476fc0f5e285f915ea4e98
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-12-17 05:23:12 +0100

    Added controller & integration tests

---
+++ spring_tests/webmvctest_autoconfiguremockmvc/src/main/java/com/webmvctest_autoconfiguremockmvc/Car.java
@@ -0,0 +1,43 @@
+package com.webmvctest_autoconfiguremockmvc;
+
+import jakarta.annotation.Nullable;
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Entity
+@Table(name = "cars")
+@Getter
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String brand;
+    String model;
+    String color;
+
+    public Car(String brand, String model, String color) {
+        this.brand = brand;
+        this.model = model;
+        this.color = color;
+    }
+
+    public Car update(@Nullable String brand, @Nullable String model, @Nullable String color) {
+        if (brand != null) {
+            this.brand = brand;
+        }
+        if (brand != null) {
+            this.model = model;
+        }
+        if (brand != null) {
+            this.color = color;
+        }
+
+        return this;
+    }
+}
