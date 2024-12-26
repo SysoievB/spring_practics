71931eb585798bb7928b4185bf32a43b0e0158d4
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-12-26 09:27:57 +0100

    Added changing default config security disable formlogin

---
+++ security/section_2_disable_form_login_and_httpbasic_auth/src/main/java/com/section_2_disable_form_login_and_httpbasic_auth/Section2DisableFormLoginAndHttpbasicAuthApplication.java
@@ -0,0 +1,13 @@
+package com.section_2_disable_form_login_and_httpbasic_auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Section2DisableFormLoginAndHttpbasicAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Section2DisableFormLoginAndHttpbasicAuthApplication.class, args);
+    }
+
+}
---
+++ security/section_2_disable_form_login_and_httpbasic_auth/src/test/java/com/section_2_disable_form_login_and_httpbasic_auth/Section2DisableFormLoginAndHttpbasicAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.section_2_disable_form_login_and_httpbasic_auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Section2DisableFormLoginAndHttpbasicAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
