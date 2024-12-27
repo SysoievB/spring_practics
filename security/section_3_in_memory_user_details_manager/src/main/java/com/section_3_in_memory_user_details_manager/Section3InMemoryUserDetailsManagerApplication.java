5d0efd506c5f21c36860ea1802c11059a484b985
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-12-27 05:57:47 +0100

    Added in memory user details manager

---
+++ security/section_3_in_memory_user_details_manager/src/main/java/com/section_3_in_memory_user_details_manager/Section3InMemoryUserDetailsManagerApplication.java
@@ -0,0 +1,13 @@
+package com.section_3_in_memory_user_details_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Section3InMemoryUserDetailsManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Section3InMemoryUserDetailsManagerApplication.class, args);
+	}
+
+}
---
+++ security/section_3_in_memory_user_details_manager/src/test/java/com/section_3_in_memory_user_details_manager/Section3InMemoryUserDetailsManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.section_3_in_memory_user_details_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Section3InMemoryUserDetailsManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
