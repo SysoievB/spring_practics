85dfbc9efa65d289844532c8c910e1d3be03f4b5
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-12-27 20:04:53 +0100

    Added managing users using a DB

---
+++ security/section_4_managing_users_with_db/src/main/java/com/section_4_managing_users_with_db/Section4ManagingUsersWithDbApplication.java
@@ -0,0 +1,13 @@
+package com.section_4_managing_users_with_db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Section4ManagingUsersWithDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Section4ManagingUsersWithDbApplication.class, args);
+    }
+
+}
---
+++ security/section_4_managing_users_with_db/src/test/java/com/section_4_managing_users_with_db/Section4ManagingUsersWithDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.section_4_managing_users_with_db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Section4ManagingUsersWithDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
