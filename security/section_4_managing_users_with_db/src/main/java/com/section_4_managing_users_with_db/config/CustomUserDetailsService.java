4533fa117753ad5501e043ae745b272bac811b18
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-12-28 07:59:55 +0100

    Added managing users using a DB

---
+++ security/section_4_managing_users_with_db/src/main/java/com/section_4_managing_users_with_db/config/CustomUserDetailsService.java
@@ -0,0 +1,33 @@
+package com.section_4_managing_users_with_db.config;
+
+import com.section_4_managing_users_with_db.repository.CustomerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class CustomUserDetailsService implements UserDetailsService {
+    private final CustomerRepository repository;
+
+    public CustomUserDetailsService(CustomerRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return repository.findByEmail(username)
+                .map(customer -> {
+                    List<GrantedAuthority> authorities = List.of(new SimpleGrantedAuthority(customer.getRole()));
+                    return new User(customer.getEmail(), customer.getPassword(), authorities);
+                })
+                .orElseThrow(() -> new UsernameNotFoundException("User details not found for the user: " + username));
+
+    }
+}
--- security/section_4_managing_users_with_db/src/main/java/com/section_4_managing_users_with_db/config/SecurityConfig.java
+++ security/section_4_managing_users_with_db/src/main/java/com/section_4_managing_users_with_db/config/SecurityConfig.java
@@ -3,39 +3,27 @@ package com.section_4_managing_users_with_db.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.password.CompromisedPasswordChecker;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(requests ->
-                requests.requestMatchers("/myAccount", "/myBalance", "/myCards", "/myLoans").authenticated()
-                        .requestMatchers("/contact", "/notices").permitAll()
-        );
-        http.formLogin(Customizer.withDefaults());
-        http.httpBasic(Customizer.withDefaults());
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards").authenticated()
+                        .requestMatchers("/notices", "/contact", "/error", "/register").permitAll());
+        http.formLogin(withDefaults());
+        http.httpBasic(withDefaults());
         return http.build();
-    }
-
-    @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails user = User.withUsername("user").password("{noop}BsSecurity@12345").authorities("read").build();
-        UserDetails admin = User.withUsername("admin")
-                .password("{bcrypt}$2a$12$lIBxO71DGD4eCZyaWcbNouz2HRpcGVlgltsHN19QWbkS6vR9F0VrG")
-                .authorities("admin")
-                .build();
-        return new InMemoryUserDetailsManager(user, admin);
     }
 
     @Bean
