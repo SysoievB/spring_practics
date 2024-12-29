4b31c46119389d06ff9d33b3fe74393b249ccac1
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-12-29 06:06:09 +0100

    Added authentication provider

---
+++ security/section_5_authentication_provider/src/main/java/com/section_5_authentication_provider/Section5AuthenticationProviderApplication.java
@@ -0,0 +1,13 @@
+package com.section_5_authentication_provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Section5AuthenticationProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Section5AuthenticationProviderApplication.class, args);
+    }
+
+}
---
+++ security/section_5_authentication_provider/src/test/java/com/section_5_authentication_provider/Section5AuthenticationProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.section_5_authentication_provider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Section5AuthenticationProviderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
