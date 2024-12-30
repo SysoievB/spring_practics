84f2b52c710758200ce2371e504c2cf5cc1f026b
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-12-30 12:03:32 +0100

    Added exception handling

---
+++ security/section_6_exception_handling/src/main/java/com/section_6_exception_handling/Section6ExceptionHandlingApplication.java
@@ -0,0 +1,13 @@
+package com.section_6_exception_handling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Section6ExceptionHandlingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Section6ExceptionHandlingApplication.class, args);
+    }
+
+}
---
+++ security/section_6_exception_handling/src/test/java/com/section_6_exception_handling/Section6ExceptionHandlingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.section_6_exception_handling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Section6ExceptionHandlingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
