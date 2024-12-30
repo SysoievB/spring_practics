e28960ca960b2a7a408212c9950aa3d304a902b1
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-12-30 16:21:47 +0100

    Added session timeout config

---
+++ security/section_6_session_timeout/src/main/java/com/section_6_session_timeout/Section6SessionTimeoutApplication.java
@@ -0,0 +1,13 @@
+package com.section_6_session_timeout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Section6SessionTimeoutApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Section6SessionTimeoutApplication.class, args);
+    }
+
+}
---
+++ security/section_6_session_timeout/src/test/java/com/section_6_session_timeout/Section6SessionTimeoutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.section_6_session_timeout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Section6SessionTimeoutApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
