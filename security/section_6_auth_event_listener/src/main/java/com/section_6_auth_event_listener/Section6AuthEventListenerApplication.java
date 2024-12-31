0e230ce196dc1fb155f9d0f7de6b60723faf45ee
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-12-31 05:48:58 +0100

    Added auth event listener

---
+++ security/section_6_auth_event_listener/src/main/java/com/section_6_auth_event_listener/Section6AuthEventListenerApplication.java
@@ -0,0 +1,13 @@
+package com.section_6_auth_event_listener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Section6AuthEventListenerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Section6AuthEventListenerApplication.class, args);
+    }
+
+}
---
+++ security/section_6_auth_event_listener/src/test/java/com/section_6_auth_event_listener/Section6AuthEventListenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.section_6_auth_event_listener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Section6AuthEventListenerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
