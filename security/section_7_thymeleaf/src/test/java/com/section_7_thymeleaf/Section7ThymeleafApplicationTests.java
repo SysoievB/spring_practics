9b72b3cbc8b5306bbbfe8a5259d5c277c30b8372
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-12-31 10:07:49 +0100

    Added thymeleaf

---
+++ security/section_7_thymeleaf/src/test/java/com/section_7_thymeleaf/Section7ThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.section_7_thymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Section7ThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
