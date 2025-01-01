e388c364984e6e71b7011a114b0a40b9bc67edcf
SysoievB <sysoiev.bogdan.ua@gmail.com>
2025-01-01 12:57:29 +0100

    Added thymeleaf

--- thymeleaf/src/main/java/com/thymeleaf/CustomerController.java
+++ thymeleaf/src/main/java/com/thymeleaf/CustomerController.java
@@ -4,10 +4,13 @@ import lombok.val;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CustomerController {
 
+    //Create a Simple HTML Template
     @GetMapping("/")//index.html
     public String index(Model model) {
         model.addAttribute("message", "Welcome to Thymeleaf!");
@@ -19,5 +22,24 @@ public class CustomerController {
         val customer = new Customer("John Doe", 30);
         model.addAttribute("customer", customer);
         return "customer";
+    }
+
+    //Forms and Validation
+    @GetMapping("/form")//form.html
+    public String form(Model model) {
+        model.addAttribute("customer", new Customer());
+        return "form";
+    }
+
+    @PostMapping("/submitForm")//form.html
+    public String submitForm(@ModelAttribute Customer customer, Model model) {
+        model.addAttribute("customer", customer);
+        return "customer";
+    }
+
+    //Layouts and Fragments
+    @GetMapping("/layout")//layout.html
+    public String layout() {
+        return "layout";
     }
 }
