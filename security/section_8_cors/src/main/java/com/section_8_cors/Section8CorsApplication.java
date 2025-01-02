bc419926579d2b40fa3ba09df97de4c66a8942a6
SysoievB <sysoiev.bogdan.ua@gmail.com>
2025-01-02 10:16:10 +0100

    Added CORs

---
+++ security/section_8_cors/src/main/java/com/section_8_cors/Section8CorsApplication.java
@@ -0,0 +1,13 @@
+package com.section_8_cors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Section8CorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Section8CorsApplication.class, args);
+	}
+
+}
---
+++ security/section_8_cors/src/test/java/com/section_8_cors/Section8CorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.section_8_cors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Section8CorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
