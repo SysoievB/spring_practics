d6adddb6110cc1cb56ecded2e18afcf80c85f571
SysoievB <sysoiev.bogdan.ua@gmail.com>
2025-01-02 14:04:10 +0100

    Added csrf

---
+++ security/section_8_csrf/src/main/java/com/section_8_csrf/Section8CsrfApplication.java
@@ -0,0 +1,13 @@
+package com.section_8_csrf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Section8CsrfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Section8CsrfApplication.class, args);
+    }
+
+}
---
+++ security/section_8_csrf/src/test/java/com/section_8_csrf/Section8CsrfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.section_8_csrf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Section8CsrfApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
