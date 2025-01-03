eec05c7a744590f25ef5274fdb5e57cd12149464
SysoievB <sysoiev.bogdan.ua@gmail.com>
2025-01-03 07:22:20 +0100

    Added authorities & roles

---
+++ security/section_9_authorities_roles/src/main/java/com/section_9_authorities_roles/Section9AuthoritiesRolesApplication.java
@@ -0,0 +1,13 @@
+package com.section_9_authorities_roles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Section9AuthoritiesRolesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Section9AuthoritiesRolesApplication.class, args);
+	}
+
+}
---
+++ security/section_9_authorities_roles/src/test/java/com/section_9_authorities_roles/Section9AuthoritiesRolesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.section_9_authorities_roles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Section9AuthoritiesRolesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
