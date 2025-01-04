544e4f49dafc4d038ad2828431fdbe59531b164b
SysoievB <sysoiev.bogdan.ua@gmail.com>
2025-01-04 04:55:13 +0100

    Added custom filter

---
+++ security/section_10_custom_filter/src/main/java/com/section_10_custom_filter/Section10CustomFilterApplication.java
@@ -0,0 +1,13 @@
+package com.section_10_custom_filter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Section10CustomFilterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Section10CustomFilterApplication.class, args);
+	}
+
+}
---
+++ security/section_10_custom_filter/src/test/java/com/section_10_custom_filter/Section10CustomFilterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.section_10_custom_filter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Section10CustomFilterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
