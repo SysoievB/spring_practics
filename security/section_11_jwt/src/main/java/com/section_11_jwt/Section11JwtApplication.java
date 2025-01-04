1e33ffa5f9c694ff84ad7a9978faec6f3f3287b0
SysoievB <sysoiev.bogdan.ua@gmail.com>
2025-01-04 14:48:33 +0100

    Added jwt

---
+++ security/section_11_jwt/src/main/java/com/section_11_jwt/Section11JwtApplication.java
@@ -0,0 +1,13 @@
+package com.section_11_jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Section11JwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Section11JwtApplication.class, args);
+    }
+
+}
---
+++ security/section_11_jwt/src/test/java/com/section_11_jwt/Section11JwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.section_11_jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Section11JwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
