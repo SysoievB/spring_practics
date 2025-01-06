e65e4dc98091c8ef1aff1657f19a31e765a6ff73
SysoievB <sysoiev.bogdan.ua@gmail.com>
2025-01-06 09:17:41 +0100

    Added @PreAuthorize & @PostAuthorize

---
+++ security/section_12_pre_post_authorize_annotations/src/main/java/com/section_12_pre_post_authorize_annotations/Section12PrePostAuthorizeAnnotationsApplication.java
@@ -0,0 +1,13 @@
+package com.section_12_pre_post_authorize_annotations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Section12PrePostAuthorizeAnnotationsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Section12PrePostAuthorizeAnnotationsApplication.class, args);
+    }
+
+}
---
+++ security/section_12_pre_post_authorize_annotations/src/test/java/com/section_12_pre_post_authorize_annotations/Section12PrePostAuthorizeAnnotationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.section_12_pre_post_authorize_annotations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Section12PrePostAuthorizeAnnotationsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
