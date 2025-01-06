4ee10e93fa752d6c77886bdfda810a68a1e5fc68
SysoievB <sysoiev.bogdan.ua@gmail.com>
2025-01-06 11:20:25 +0100

    Added @PreFilter & @PostFilter

---
+++ security/section_12_pre_post_filter_annotations/src/main/java/com/section_12_pre_post_filter_annotations/Section12PrePostFilterAnnotationsApplication.java
@@ -0,0 +1,13 @@
+package com.section_12_pre_post_filter_annotations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Section12PrePostFilterAnnotationsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Section12PrePostFilterAnnotationsApplication.class, args);
+    }
+
+}
---
+++ security/section_12_pre_post_filter_annotations/src/test/java/com/section_12_pre_post_filter_annotations/Section12PrePostFilterAnnotationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.section_12_pre_post_filter_annotations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Section12PrePostFilterAnnotationsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
