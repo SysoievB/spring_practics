681337ee2f3c289273d1a064270047f7f16080db
SysoievB <sysoiev.bogdan.ua@gmail.com>
2025-01-07 18:38:13 +0100

    Added oauth2

---
+++ security/section_13_oauth2/src/main/java/com/section_13_oauth2/Section13Oauth2Application.java
@@ -0,0 +1,13 @@
+package com.section_13_oauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Section13Oauth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Section13Oauth2Application.class, args);
+	}
+
+}
