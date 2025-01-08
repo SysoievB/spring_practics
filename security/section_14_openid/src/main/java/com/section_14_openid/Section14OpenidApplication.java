494f85402dabf62bf49f796ee80bd5498cc567a2
SysoievB <sysoiev.bogdan.ua@gmail.com>
2025-01-08 05:51:57 +0100

    Added openId

---
+++ security/section_14_openid/src/main/java/com/section_14_openid/Section14OpenidApplication.java
@@ -0,0 +1,13 @@
+package com.section_14_openid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Section14OpenidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Section14OpenidApplication.class, args);
+	}
+
+}
---
+++ security/section_14_openid/src/test/java/com/section_14_openid/Section14OpenidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.section_14_openid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Section14OpenidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
