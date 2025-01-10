9b150cca5ad61d32be303eee87d289076e8e0378
SysoievB <sysoiev.bogdan.ua@gmail.com>
2025-01-10 11:16:54 +0100

    Added ms sql with docker

---
+++ liquibase_mssql/src/main/java/com/liquibase_mssql/Book.java
@@ -0,0 +1,35 @@
+package com.liquibase_mssql;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "book")
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer id;
+
+    @Column(name = "book_uuid", columnDefinition = "CHAR(36)")
+    UUID bookUuid;
+
+    String title;
+
+    String author;
+
+    String publisher;
+
+    @Column(name = "is_last")
+    boolean last;
+}
+
---
+++ liquibase_mssql/src/test/java/com/liquibase_mssql/LiquibaseMssqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liquibase_mssql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiquibaseMssqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
