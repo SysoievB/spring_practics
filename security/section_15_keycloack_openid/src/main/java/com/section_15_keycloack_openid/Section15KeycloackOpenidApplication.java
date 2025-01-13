eddbd33d7d56d6a7a6baa1dcc1466263a6703d18
SysoievB <sysoiev.bogdan.ua@gmail.com>
2025-01-13 19:08:06 +0100

    Added keykloack openId

---
+++ security/section_15_keycloack_openid/src/main/java/com/section_15_keycloack_openid/Section15KeycloackOpenidApplication.java
@@ -0,0 +1,13 @@
+package com.section_15_keycloack_openid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Section15KeycloackOpenidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Section15KeycloackOpenidApplication.class, args);
+	}
+
+}
---
+++ security/section_15_keycloack_openid/src/test/java/com/section_15_keycloack_openid/Section15KeycloackOpenidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.section_15_keycloack_openid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Section15KeycloackOpenidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
