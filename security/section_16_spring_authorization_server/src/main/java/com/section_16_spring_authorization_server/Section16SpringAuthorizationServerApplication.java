d0daa34fb9aa2ad81884e88b69f929728fed8e36
SysoievB <sysoiev.bogdan.ua@gmail.com>
2025-01-15 06:22:07 +0100

    Added spring authorization server

---
+++ security/section_16_spring_authorization_server/src/main/java/com/section_16_spring_authorization_server/Section16SpringAuthorizationServerApplication.java
@@ -0,0 +1,13 @@
+package com.section_16_spring_authorization_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Section16SpringAuthorizationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Section16SpringAuthorizationServerApplication.class, args);
+	}
+
+}
---
+++ security/section_16_spring_authorization_server/src/test/java/com/section_16_spring_authorization_server/Section16SpringAuthorizationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.section_16_spring_authorization_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Section16SpringAuthorizationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
