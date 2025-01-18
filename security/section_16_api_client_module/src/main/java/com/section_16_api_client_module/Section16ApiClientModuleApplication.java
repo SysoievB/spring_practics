eac877ed19338eeb9a5868dfd687aeb9417b0777
SysoievB <sysoiev.bogdan.ua@gmail.com>
2025-01-18 06:20:00 +0100

    Added api client module

---
+++ security/section_16_api_client_module/src/main/java/com/section_16_api_client_module/Section16ApiClientModuleApplication.java
@@ -0,0 +1,13 @@
+package com.section_16_api_client_module;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Section16ApiClientModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Section16ApiClientModuleApplication.class, args);
+    }
+
+}
---
+++ security/section_16_api_client_module/src/test/java/com/section_16_api_client_module/Section16ApiClientModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.section_16_api_client_module;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Section16ApiClientModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
