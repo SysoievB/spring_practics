62b1aef31a393369c2ac0a8c032cf4d19603463f
SysoievB <sysoiev.bogdan.ua@gmail.com>
2025-01-28 05:18:57 +0100

    Added one time token

---
+++ security/one_time_token/src/test/java/com/one_time_token/OneTimeTokenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.one_time_token;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneTimeTokenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
