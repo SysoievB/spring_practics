5c1168ae1220ff310d6f9793333f3c8a795636ba
SysoievB <sysoiev.bogdan.ua@gmail.com>
2025-02-08 05:35:25 +0100

    Added SQLInsert

---
+++ data_jpa/sql_insert_annotation/src/main/java/com/sql_insert_annotation/SqlInsertAnnotationApplication.java
@@ -0,0 +1,13 @@
+package com.sql_insert_annotation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlInsertAnnotationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqlInsertAnnotationApplication.class, args);
+	}
+
+}
---
+++ data_jpa/sql_insert_annotation/src/test/java/com/sql_insert_annotation/SqlInsertAnnotationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sql_insert_annotation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqlInsertAnnotationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
